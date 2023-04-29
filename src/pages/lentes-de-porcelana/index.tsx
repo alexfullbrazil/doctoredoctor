0512a8bce92f2e9171497a607e5bf5c666b8ca0c
AlexFull <alexfull2@gmail.com>
2023-04-29 15:25:30 -0300

    removed next seo

--- src/pages/lentes-de-porcelana/index.tsx
+++ src/pages/lentes-de-porcelana/index.tsx
@@ -5,34 +5,39 @@ import { Hero } from "@/components/hero";
 import { Procedures } from "@/components/procedures";
 import { Testimonials } from "@/components/testimonials";
 import { NextSeo } from "next-seo";
+import Head from "next/head";
 
 export default function LentesDePorcelana() {
   return (
     <>
-      <NextSeo
-        title="Lentes de Contato Dental de Porcelana | Doctor&doctor"
-        description="Com as Lentes de Contato Dental de Porcelana é possível recuperar a beleza e a genuinidade do seu sorriso."
-        canonical="https://esteticadental.doctoredoctor.com.br/lentes-de-porcelana"
-        openGraph={{
-          url: "https://esteticadental.doctoredoctor.com.br/lentes-de-porcelana",
-          title: "Lentes de Contato Dental de Porcelana | Doctor&doctor",
-          description:
-            "Com as Lentes de Contato Dental de Porcelana é possível recuperar a beleza e a genuinidade do seu sorriso.",
-          images: [
-            {
-              url: "https://esteticadental.doctoredoctor.com.br/assets/images/og/og-image-lentes-de-porcelana.jpg",
-              width: 1200,
-              height: 600,
-              alt: "Lentes de Contato Dental de Porcelana | Doctor&doctor",
-              type: "image/jpeg",
-            },
-            {
-              url: "https://esteticadental.doctoredoctor.com.br/assets/images/og/og-image-lentes-de-porcelana.jpg",
-            },
-          ],
-          siteName: "Doctor&doctor",
-        }}
-      />
+      <Head>
+        <title>Lentes de Contato Dental de Porcelana | Doctor&doctor</title>
+        <meta property="og:site_name" content="Doctor&doctor" />
+        <meta
+          property="og:title"
+          content="Lentes de Contato Dental de Porcelana | Doctor&doctor"
+        />
+        <meta
+          property="og:description"
+          content="Com as Lentes de Contato Dental de Porcelana é possível recuperar a beleza e a genuinidade do seu sorriso."
+        />
+        <meta
+          property="og:url"
+          content="https://esteticadental.doctoredoctor.com.br/lentes-de-porcelana"
+        />
+        <meta property="og:type" content="article" />
+        <meta
+          property="og:image"
+          content="https://esteticadental.doctoredoctor.com.br/assets/images/og/og-image-lentes-de-porcelana.jpg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://esteticadental.doctoredoctor.com.br/assets/images/og/og-image-lentes-de-porcelana.jpg"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
+      </Head>
+
       <main>
         <Hero />
         <About />
