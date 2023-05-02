8221bb15a20f56f7c8ad31104f574a4ca2d65e6e
AlexFull <alexfull2@gmail.com>
2023-05-02 14:05:50 -0300

    added verel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import "@fontsource/inter/700.css";
 import "@fontsource/inter/300.css";
 
 import "@/styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
