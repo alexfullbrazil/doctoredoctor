f3470afe3ceb4b9164750d48834c86ba10c81f5b
AlexFull <alexfull2@gmail.com>
2023-05-08 14:53:36 -0300

    updated google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,9 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
+      <Head>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
           (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
           m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -15,7 +15,9 @@ export default function Document() {
           ga('create', 'AW-1070255284', 'auto');
           ga('send', 'pageview');
         `}
-      </Script>
+        </Script>
+      </Head>
+
       <body>
         <Main />
         <NextScript />
