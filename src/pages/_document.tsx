8ce5fb435b200266ccad6eae841a1318170699d2
AlexFull <alexfull2@gmail.com>
2023-05-11 02:11:38 -0300

    update analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head>
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'AW-1070255284', 'auto');
-          ga('send', 'pageview');
-        `}
-        </Script>
-      </Head>
-
+      <Head />
       <body>
         <Main />
         <NextScript />
--- src/pages/lentes-de-porcelana/index.tsx
+++ src/pages/lentes-de-porcelana/index.tsx
@@ -7,10 +7,24 @@ import LentesDePorcelanaHead from "@/components/heads/lentes-de-porcelana";
 import { Hero } from "@/components/hero";
 import { Procedures } from "@/components/procedures";
 import { Testimonials } from "@/components/testimonials";
+import Head from "next/head";
+import Script from "next/script";
 
 export default function LentesDePorcelana() {
   return (
     <>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'AW-1070255284', 'auto');
+          ga('send', 'pageview');
+        `}
+      </Script>
+
       <LentesDePorcelanaHead />
       <Hero />
       <About />
