e55d3252db79b318d2deecf2b9e889a014a26cf3
AlexFull Brazil <alexfull2@gmail.com>
2024-07-09 11:41:16 -0300

    remove vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type { AppProps } from "next/app"
-import { Analytics } from "@vercel/analytics/react"
 import { GoogleTagManager } from '@next/third-parties/google'
 import "@fontsource/inter/700.css"
 import "@fontsource/inter/300.css"
@@ -11,7 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
       <GoogleTagManager gtmId="GTM-T5CF3H4" />
     </>
   )
