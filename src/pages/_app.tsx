33a3d5584ef8d3f97e345784c335f21054ce7496
AlexFull Brazil <alexfull2@gmail.com>
2024-07-09 11:30:44 -0300

    added GoogleTagManager @next/third-parties/google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,18 @@
-import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
-import "@fontsource/inter/700.css";
-import "@fontsource/inter/300.css";
-import "@fontsource/inter/400.css";
+import type { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
+import { GoogleTagManager } from '@next/third-parties/google'
+import "@fontsource/inter/700.css"
+import "@fontsource/inter/300.css"
+import "@fontsource/inter/400.css"
 
-import "@/styles/globals.scss";
+import "@/styles/globals.scss"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
       <Analytics />
+      <GoogleTagManager gtmId="GTM-T5CF3H4" />
     </>
-  );
+  )
 }
